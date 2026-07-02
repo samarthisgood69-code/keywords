72d7f54271a9145a707e69a792823671ad905853
samarthisgood69 <samarthisgood69@gmail.com>
2026-07-02 19:38:08 +0530

    initial commit

---
+++ pass.py
@@ -0,0 +1,13 @@
+for y in range(15):
+    if y %10==0:
+        print("twist")
+
+    elif y%5==0:
+        pass
+    elif y%3==0:
+        print("buss")
+    elif y%2==0:
+        print("fizz")
+    else:
+        print(y)
+        
